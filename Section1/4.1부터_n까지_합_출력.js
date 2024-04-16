edb14eb93e4e4f425e801e0f569e85fb12d1f398
Robin <kal6529@gmail.com>
2024-04-16 14:33:58 +0900

    연필 갯수, 1부터 n 까지 합 출력 문제 풀이

---
+++ Section1/4.1부터_n까지_합_출력.js
@@ -0,0 +1,21 @@
+function solution(n) {
+  let answer = 0;
+  for (let i = 1; i <= n; i++) {
+    answer += i;
+  }
+  return answer;
+}
+
+console.log(solution(6));
+
+/**
+ * 1부터 n까지 합 출력
+ *
+ * 자연수 N이 입력되면 1부터 N까지의 합을 출력하는 프로그램을 작성하세요.
+ *
+ * ▣ 입력설명
+ * 첫 번째 줄에 20이하의 자연수 N이 입력된다.
+ *
+ * ▣ 출력설명
+ * 첫 번째 줄에 1부터 N까지의 합을 출력한다.
+ */
